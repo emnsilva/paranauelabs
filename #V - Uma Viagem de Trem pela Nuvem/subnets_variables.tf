21d128b5f16b3c73b2d476343fa2965f86c310b7
Emerson <emnsilva@hotmail.com>
2025-08-07 19:47:40 -0300

    Update subnets_variables.tf

--- #V - Uma Viagem de Trem pela Nuvem/subnets_variables.tf
+++ #V - Uma Viagem de Trem pela Nuvem/subnets_variables.tf
@@ -1,11 +1,13 @@
-# subnets_variables.tf: Configurações comuns para subnets
+# subnet_variables.tf
+variable "subnet_colors" {
+  description = "Ordem e nomes das subnets baseadas em cores"
+  type        = list(string)
+  default     = ["red", "green", "blue", "yellow", "pink", "gold", "silver", "white"]
+}
 
-variable "subnet_settings" {
-  description = "Configurações padrão para todas as subnets"
-  type = object({
-    public_ip_on_launch = bool
-  })
-  default = {
-    public_ip_on_launch = true
-  }
+variable "subnet_cidr_blocks" {
+  description = "Faixas de IPs para cada subnet (8 blocos /24 por VPC)"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24",
+                 "10.0.5.0/24", "10.0.6.0/24", "10.0.7.0/24", "10.0.8.0/24"]
 }
