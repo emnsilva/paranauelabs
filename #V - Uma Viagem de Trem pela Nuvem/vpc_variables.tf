f03dbca0c1eb5cf0a6490c2ba44c155131b633c2
Emerson <emnsilva@hotmail.com>
2025-08-07 17:15:04 -0300

    Update vpc_variables.tf

--- #V - Uma Viagem de Trem pela Nuvem/vpc_variables.tf
+++ #V - Uma Viagem de Trem pela Nuvem/vpc_variables.tf
@@ -8,7 +8,7 @@ variable "vpcs" {
   }))
   
   default = {
-    primary = {  # VPC principal de produção
+    main = {  # VPC principal de produção
       region     = "sa-east-1"
       cidr_block = "10.0.0.0/16"
     }
