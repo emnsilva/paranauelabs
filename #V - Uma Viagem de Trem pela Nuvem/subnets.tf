d196ccc471f446ed761efe6fd8bec77d0933297d
Emerson <emnsilva@hotmail.com>
2025-08-07 18:31:41 -0300

    Create subnets.tf

---
+++ #V - Uma Viagem de Trem pela Nuvem/subnets.tf
@@ -0,0 +1,29 @@
+# subnets.tf: Cria 8 subnets públicas em cada VPC (main e backup) como espelhos exatos
+
+# Subnets na VPC Main (sa-east-1)
+resource "aws_subnet" "main_public" {
+  provider                = aws.main
+  count                   = 8
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpcs["main"].cidr_block, 4, count.index)
+  availability_zone       = "${var.vpcs["main"].region}${substr("abcdefgh", count.index, 1)}"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "main-public-${count.index + 1}"
+  }
+}
+
+# Subnets na VPC Backup (us-east-1) - Espelho exato
+resource "aws_subnet" "backup_public" {
+  provider                = aws.backup
+  count                   = 8
+  vpc_id                  = aws_vpc.backup.id
+  cidr_block              = cidrsubnet(var.vpcs["backup"].cidr_block, 4, count.index) # Mesmos índices
+  availability_zone       = "${var.vpcs["backup"].region}${substr("abcdefgh", count.index, 1)}"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "backup-public-${count.index + 1}"
+  }
+}
