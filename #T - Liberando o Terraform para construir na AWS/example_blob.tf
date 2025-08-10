0e7c79759c9b50a06e3e0bc3b29c4c5456a406c1
Emerson <emnsilva@hotmail.com>
2025-08-10 20:01:48 -0300

    Create example_blob.tf

---
+++ #T - Liberando o Terraform para construir na AWS/example_blob.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Cria um grupo de recursos
+resource "azurerm_resource_group" "main" {
+  name     = "main-blob-storage"
+  location = "brazilsouth"
+}
+
+# Cria a conta de armazenamento (equivalente ao bucket S3)
+resource "azurerm_storage_account" "example" {
+  name                     = "main_storage"  # Nome deve ser único globalmente
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"  # Localmente redundante
+}
+
+# Cria um container (similar a uma pasta no bucket S3)
+resource "azurerm_storage_container" "main" {
+  name                  = "main-container"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"  # Pode ser "blob" ou "container" para acesso público
+}
