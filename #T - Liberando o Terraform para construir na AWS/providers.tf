228b5ee40d8c923d7945446fa466092d934e27b5
Emerson <emnsilva@hotmail.com>
2025-08-10 20:27:23 -0300

    Create providers.tf

---
+++ #T - Liberando o Terraform para construir na AWS/providers.tf
@@ -0,0 +1,13 @@
+# providers.tf (ou adicione isso em um dos arquivos existentes)
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
