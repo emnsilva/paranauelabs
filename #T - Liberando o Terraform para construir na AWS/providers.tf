1a5b1730d497429212ca322d46b203cc01e41976
Emerson <emnsilva@hotmail.com>
2025-08-15 15:49:00 -0300

    Update providers.tf

--- #T - Liberando o Terraform para construir na AWS/providers.tf
+++ #T - Liberando o Terraform para construir na AWS/providers.tf
@@ -6,29 +6,38 @@ terraform {
   }
 }
 
-# Configuração dos providers
+# Configuração PRIMÁRIA (sem alias)
 provider "aws" {
+  alias = "primary"
   region = local.regions.aws[var.CLOUD_PRIMARY_REGION]
-  
-  # Provider secundário (para failover/redundância)
-  alias = "secondary"
+}
+
+provider "azurerm" {
+  alias = "primary"
+  features {}
+  location = local.regions.azure[var.CLOUD_PRIMARY_REGION]
+}
+
+provider "google" {
+  alias = "primary"
+  project = var.GCP_PROJECT_ID
+  region  = local.regions.gcp[var.CLOUD_PRIMARY_REGION]
+}
+
+# Configuração SECUNDÁRIA (com alias explícito)
+provider "aws" {
+  alias  = "secondary"
   region = local.regions.aws[var.CLOUD_SECONDARY_REGION]
 }
 
 provider "azurerm" {
+  alias    = "secondary"
   features {}
-  location = local.regions.azure[var.CLOUD_PRIMARY_REGION]
-  
-  # Provider secundário
-  alias = "secondary"
   location = local.regions.azure[var.CLOUD_SECONDARY_REGION]
 }
 
 provider "google" {
+  alias   = "secondary"
   project = var.GCP_PROJECT_ID
-  region = local.regions.gcp[var.CLOUD_PRIMARY_REGION]
-  
-  # Provider secundário
-  alias = "secondary"
-  region = local.regions.gcp[var.CLOUD_SECONDARY_REGION]
+  region  = local.regions.gcp[var.CLOUD_SECONDARY_REGION]
 }
