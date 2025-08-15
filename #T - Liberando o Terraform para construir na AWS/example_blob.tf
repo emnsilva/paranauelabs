795aeb47df71d94ac0078992d06283db1a689342
Emerson <emnsilva@hotmail.com>
2025-08-14 21:16:00 -0300

    Update example_blob.tf

--- #T - Liberando o Terraform para construir na AWS/example_blob.tf
+++ #T - Liberando o Terraform para construir na AWS/example_blob.tf
@@ -2,24 +2,46 @@ provider "azurerm" {
   features {}
 }
 
-# Cria um grupo de recursos
-resource "azurerm_resource_group" "main" {
-  name     = "main-blob-storage"
+# Grupo de recursos na região primária (Brazil South)
+resource "azurerm_resource_group" "main_br" {
+  name     = "main-blob-storage-br"
   location = "brazilsouth"
 }
 
-# Cria a conta de armazenamento com o nome "paranaublob"
-resource "azurerm_storage_account" "main" {
-  name                     = "paranaublob"  # Nome alterado conforme solicitado
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"  # Localmente redundante
+# Grupo de recursos na região secundária (East US)
+resource "azurerm_resource_group" "main_us" {
+  name     = "main-blob-storage-us"
+  location = "eastus"
 }
 
-# Cria um container (similar a uma pasta no bucket S3)
-resource "azurerm_storage_container" "main" {
-  name                  = "main-container"
-  storage_account_name  = azurerm_storage_account.main.name  # Referência corrigida
+# Conta de armazenamento na região primária
+resource "azurerm_storage_account" "main_br" {
+  name                     = "paranaublobbr"  # Nome único para Brazil
+  resource_group_name      = azurerm_resource_group.main_br.name
+  location                 = azurerm_resource_group.main_br.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Conta de armazenamento na região secundária
+resource "azurerm_storage_account" "main_us" {
+  name                     = "paranaublobus"  # Nome único para East US
+  resource_group_name      = azurerm_resource_group.main_us.name
+  location                 = azurerm_resource_group.main_us.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Container na região primária
+resource "azurerm_storage_container" "main_br" {
+  name                  = "main-container-br"
+  storage_account_name  = azurerm_storage_account.main_br.name
+  container_access_type = "private"
+}
+
+# Container na região secundária
+resource "azurerm_storage_container" "main_us" {
+  name                  = "main-container-us"
+  storage_account_name  = azurerm_storage_account.main_us.name
   container_access_type = "private"
 }
