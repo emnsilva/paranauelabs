b6c85f7e34bd48ca5f7acafa8fbc7849613a3372
Emerson <emnsilva@hotmail.com>
2025-08-15 12:14:21 -0300

    Update example_storage.tf

--- #T - Liberando o Terraform para construir na AWS/example_storage.tf
+++ #T - Liberando o Terraform para construir na AWS/example_storage.tf
@@ -1,32 +1,20 @@
-# Declarações OBRIGATÓRIAS (mesmo no Terraform Cloud)
-variable "GOOGLE_CREDENTIALS_B64" {
-  type        = string
-  description = "Conteúdo do JSON da Service Account em Base64"
-  sensitive   = true
-}
-
-variable "GCP_PROJECT_ID" {
-  type        = string
-  description = "ID do projeto no Google Cloud"
-}
-
 # Configuração do Provider
 provider "google" {
   credentials = base64decode(var.GOOGLE_CREDENTIALS_B64)
   project     = var.GCP_PROJECT_ID
-  region      = "southamerica-east1" # Região fixa (Brasil)
+  region      = local.region_mapping[var.PRIMARY_REGION_ALIAS].gcp
 }
 
 # Bucket no Brasil
 resource "google_storage_bucket" "brasil" {
-  name          = "${var.GCP_PROJECT_ID}-bucket-br" # Nome dinâmico
-  location      = "southamerica-east1"
+  name          = "${var.GCP_PROJECT_ID}-bucket-br"
+  location      = local.region_mapping[var.PRIMARY_REGION_ALIAS].gcp
   storage_class = "STANDARD"
 }
 
 # Bucket nos USA
 resource "google_storage_bucket" "usa" {
-  name          = "${var.GCP_PROJECT_ID}-bucket-br" # Nome dinâmico
-  location      = "us-east1"
+  name          = "${var.GCP_PROJECT_ID}-bucket-us"
+  location      = local.region_mapping[var.SECONDARY_REGION_ALIAS].gcp
   storage_class = "STANDARD"
 }
