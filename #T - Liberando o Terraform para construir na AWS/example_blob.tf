e70aa0a4a022a3110a37a69849c2b9f8467ea981
Emerson <emnsilva@hotmail.com>
2025-08-15 17:16:52 -0300

    Update example_blob.tf

--- #T - Liberando o Terraform para construir na AWS/example_blob.tf
+++ #T - Liberando o Terraform para construir na AWS/example_blob.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "secondary" {
 }
 
 resource "azurerm_storage_account" "primary" {
-  name                     = "paranaueblob${replace(lower(var.ARM_PRIMARY_REGION), "/[^a-z0-9]/", ""}"
+  name                     = "paranaueblob${replace(lower(var.ARM_PRIMARY_REGION), "[^a-z0-9]", "")}"
   resource_group_name      = azurerm_resource_group.primary.name
   location                 = azurerm_resource_group.primary.location
   account_tier             = "Standard"
@@ -18,7 +18,7 @@ resource "azurerm_storage_account" "primary" {
 }
 
 resource "azurerm_storage_account" "secondary" {
-  name                     = "paranaueblob${replace(lower(var.ARM_SECONDARY_REGION), "/[^a-z0-9]/", ""}"
+  name                     = "paranaueblob${replace(lower(var.ARM_SECONDARY_REGION), "[^a-z0-9]", "")}"
   resource_group_name      = azurerm_resource_group.secondary.name
   location                 = azurerm_resource_group.secondary.location
   account_tier             = "Standard"
@@ -26,13 +26,13 @@ resource "azurerm_storage_account" "secondary" {
 }
 
 resource "azurerm_storage_container" "primary" {
-  name                  = "main-container-${lower(var.ARM_PRIMARY_REGION)}"
+  name                  = "primary-container-${lower(var.ARM_PRIMARY_REGION)}"
   storage_account_id    = azurerm_storage_account.primary.id
   container_access_type = "private"
 }
 
 resource "azurerm_storage_container" "secondary" {
-  name                  = "main-container-${lower(var.ARM_SECONDARY_REGION)}"
+  name                  = "secondary-container-${lower(var.ARM_SECONDARY_REGION)}"
   storage_account_id    = azurerm_storage_account.secondary.id
   container_access_type = "private"
 }
