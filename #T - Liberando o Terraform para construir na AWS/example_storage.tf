902fdeb15d0b7b5bfcbf3114550bc3dcbc2074bb
Emerson <emnsilva@hotmail.com>
2025-08-15 12:35:49 -0300

    Update example_storage.tf

--- #T - Liberando o Terraform para construir na AWS/example_storage.tf
+++ #T - Liberando o Terraform para construir na AWS/example_storage.tf
@@ -2,19 +2,19 @@
 provider "google" {
   credentials = base64decode(var.GOOGLE_CREDENTIALS_B64)
   project     = var.GCP_PROJECT_ID
-  region      = locals.region_mapping[var.CLOUD_PRIMARY_REGION].gcp
+  region      = local.region_mapping[var.CLOUD_PRIMARY_REGION].gcp
 }
 
 # Bucket no Brasil
 resource "google_storage_bucket" "brasil" {
   name          = "${var.GCP_PROJECT_ID}-bucket-br"
-  location      = locals.region_mapping[var.CLOUD_PRIMARY_REGION].gcp
+  location      = local.region_mapping[var.CLOUD_PRIMARY_REGION].gcp
   storage_class = "STANDARD"
 }
 
 # Bucket nos USA
 resource "google_storage_bucket" "usa" {
   name          = "${var.GCP_PROJECT_ID}-bucket-us"
-  location      = locals.region_mapping[var.CLOUD_SECONDARY_REGION].gcp
+  location      = local.region_mapping[var.CLOUD_SECONDARY_REGION].gcp
   storage_class = "STANDARD"
 }
