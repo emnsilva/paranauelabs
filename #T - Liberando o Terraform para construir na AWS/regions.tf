8f510cef46e789a7f8d6a3f2aedf94887b3b4f67
Emerson <emnsilva@hotmail.com>
2025-08-15 12:06:56 -0300

    Create regions.tf

---
+++ #T - Liberando o Terraform para construir na AWS/regions.tf
@@ -0,0 +1,14 @@
+regions {
+  region_mapping = {
+    southamerica = {
+      aws   = "sa-east-1",
+      azure = "brazilsouth",
+      gcp   = "southamerica-east1"
+    },
+    eastus = {
+      aws   = "us-east-1",
+      azure = "eastus",
+      gcp   = "us-east1"
+    }
+  }
+}
