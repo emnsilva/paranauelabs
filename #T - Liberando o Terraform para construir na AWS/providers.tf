07b1776dcaa7220212789b4763872b1ab6867aab
Emerson <emnsilva@hotmail.com>
2025-08-15 15:56:10 -0300

    Update providers.tf

--- #T - Liberando o Terraform para construir na AWS/providers.tf
+++ #T - Liberando o Terraform para construir na AWS/providers.tf
@@ -5,36 +5,3 @@ terraform {
     google = { source = "hashicorp/google", version = "~> 6.48.0" }
   }
 }
-
-# Configuração PRIMÁRIA
-provider "aws" {
-  region = local.regions.aws[var.CLOUD_PRIMARY_REGION]
-}
-
-provider "azurerm" {
-  features {}
-  location = local.regions.azure[var.CLOUD_PRIMARY_REGION]
-}
-
-provider "google" {
-  project = var.GCP_PROJECT_ID
-  region  = local.regions.gcp[var.CLOUD_PRIMARY_REGION]
-}
-
-# Configuração SECUNDÁRIA
-provider "aws" {
-  alias  = "secondary"
-  region = local.regions.aws[var.CLOUD_SECONDARY_REGION]
-}
-
-provider "azurerm" {
-  alias    = "secondary"
-  features {}
-  location = local.regions.azure[var.CLOUD_SECONDARY_REGION]
-}
-
-provider "google" {
-  alias   = "secondary"
-  project = var.GCP_PROJECT_ID
-  region  = local.regions.gcp[var.CLOUD_SECONDARY_REGION]
-}
