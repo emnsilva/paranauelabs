6d36ec1bc2b03594124dc752e1d5444baa69b24e
Emerson <emnsilva@hotmail.com>
2025-08-16 21:52:41 -0300

    Create example_s3.tf

---
+++ #T - Terraform Airlines/example_s3.tf
@@ -0,0 +1,27 @@
+variable "AWS_REGION_PRIMARY" {}
+variable "AWS_REGION_SECONDARY" {}
+
+# Configuração dos providers AWS com variáveis dinâmicas
+provider "aws" {
+  alias  = "primary"
+  region = var.AWS_REGION_PRIMARY
+}
+
+provider "aws" {
+  alias  = "secondary"
+  region = var.AWS_REGION_SECONDARY
+}
+
+# Bucket na região primária
+resource "aws_s3_bucket" "primary_bucket" {
+  provider      = aws.primary
+  bucket        = "primary-bucket"  # Substitua por um nome único global
+  force_destroy = true  # Permite deletar o bucket mesmo com conteúdo (cuidado!)
+}
+
+# Bucket na região secundária
+resource "aws_s3_bucket" "secondary_bucket" {
+  provider      = aws.secondary
+  bucket        = "secondary-bucket"  # Nome único global
+  force_destroy = true
+}
