11b76a9d291f1bb876b16f4ca1ac51a589c0b950
Emerson <emnsilva@hotmail.com>
2025-08-16 21:53:29 -0300

    Delete #T - Liberando o Terraform para construir na AWS directory

--- #T - Liberando o Terraform para construir na AWS/providers.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 6.9.0" }
-    azurerm = { source = "hashicorp/azurerm", version = "~> 4.40.0" }
-    google = { source = "hashicorp/google", version = "~> 6.48.0" }
-  }
-}
