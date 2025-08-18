e4c1c53fd83f7d1553acb0950dad7a93568f4041
Emerson <emnsilva@hotmail.com>
2025-08-18 16:35:24 -0300

    Update example_storage.tf

--- #T - Terraform Airlines/example_storage.tf
+++ #T - Terraform Airlines/example_storage.tf
@@ -1,22 +1,24 @@
-variable "GCP_PROJECT" {}
 variable "GCP_PRIMARY_REGION" {}
 variable "GCP_SECONDARY_REGION" {}
 
 # Provider configurado para usar OIDC automaticamente no TFC
 provider "google" {
-  project = var.GCP_PROJECT
   region  = var.GCP_PRIMARY_REGION
+}
+
+provider "google" {
+  region  = var.GCP_SECONDARY_REGION
 }
 
 # Recursos (nomes dinâmicos baseados nas variáveis)
 resource "google_storage_bucket" "primary" {
-  name          = "${var.GCP_PROJECT}-primary-${var.GCP_PRIMARY_REGION}"
+  provider      = google.primary
   location      = var.GCP_PRIMARY_REGION
   storage_class = "STANDARD"
 }
 
 resource "google_storage_bucket" "secondary" {
-  name          = "${var.GCP_PROJECT}-secondary-${var.GCP_SECONDARY_REGION}"
+  provider      = google.primary
   location      = var.GCP_SECONDARY_REGION
   storage_class = "STANDARD"
 }
