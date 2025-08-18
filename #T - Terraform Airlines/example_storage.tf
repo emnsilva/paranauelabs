0422dfd0ef26983b49cd45bdaf5f5e81bf58cb52
Emerson <emnsilva@hotmail.com>
2025-08-18 16:41:33 -0300

    Update example_storage.tf

--- #T - Terraform Airlines/example_storage.tf
+++ #T - Terraform Airlines/example_storage.tf
@@ -14,12 +14,14 @@ provider "google" {
 
 # Recursos (nomes dinâmicos baseados nas variáveis)
 resource "google_storage_bucket" "primary" {
+  name          = "primary-storage"
   provider      = google.primary
   location      = var.GCP_PRIMARY_REGION
   storage_class = "STANDARD"
 }
 
 resource "google_storage_bucket" "secondary" {
+  name          = "secondary-storage"
   provider      = google.primary
   location      = var.GCP_SECONDARY_REGION
   storage_class = "STANDARD"
