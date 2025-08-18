9238b60e35698ff102a0ac681cb369de5eb1d709
Emerson <emnsilva@hotmail.com>
2025-08-18 18:23:48 -0300

    Update example_blob.tf

--- #T - Terraform Airlines/example_blob.tf
+++ #T - Terraform Airlines/example_blob.tf
@@ -2,22 +2,37 @@ variable "ARM_PRIMARY_REGION" {}
 variable "ARM_SECONDARY_REGION" {}
 
 provider "azurerm" {
-  features {}
+  alias  = "primary"
+  region = var.ARM_PRIMARY_REGION
+}
+
+provider "azurerm" {
+  alias  = "secondary"
+  region = var.ARM_SECONDARY_REGION
+}
+
+
+# Configurações principais usando variáveis do Terraform Cloud
+resource "azurerm_resource_group" "primary" {
+  name     = "primary-blob-storage"
+  provider = azurerm.primary
+  location = var.ARM_PRIMARY_REGION
 }
 
 # Configurações principais usando variáveis do Terraform Cloud
 resource "azurerm_resource_group" "primary" {
-  name     = "primary-blob-storage-${lower(var.ARM_PRIMARY_REGION)}"
-  location = var.ARM_PRIMARY_REGION
+  name     = "secondary-blob-storage"
+  provider = azurerm.secondary
+  location = var.ARM_SECONDARY_REGION
 }
 
 resource "azurerm_resource_group" "secondary" {
-  name     = "secondary-blob-storage-${lower(var.ARM_SECONDARY_REGION)}"
+  name     = "secondary-blob-storage"
   location = var.ARM_SECONDARY_REGION
 }
 
 resource "azurerm_storage_account" "primary" {
-  name                     = "paranaueblob${replace(lower(var.ARM_PRIMARY_REGION), "[^a-z0-9]", "")}"
+  name                     = "blob-primary"
   resource_group_name      = azurerm_resource_group.primary.name
   location                 = azurerm_resource_group.primary.location
   account_tier             = "Standard"
@@ -25,7 +40,7 @@ resource "azurerm_storage_account" "primary" {
 }
 
 resource "azurerm_storage_account" "secondary" {
-  name                     = "paranaueblob${replace(lower(var.ARM_SECONDARY_REGION), "[^a-z0-9]", "")}"
+  name                     = "blob-secondary"
   resource_group_name      = azurerm_resource_group.secondary.name
   location                 = azurerm_resource_group.secondary.location
   account_tier             = "Standard"
@@ -33,13 +48,13 @@ resource "azurerm_storage_account" "secondary" {
 }
 
 resource "azurerm_storage_container" "primary" {
-  name                  = "primary-container-${lower(var.ARM_PRIMARY_REGION)}"
+  name                  = "primary-container"
   storage_account_id    = azurerm_storage_account.primary.id
   container_access_type = "private"
 }
 
 resource "azurerm_storage_container" "secondary" {
-  name                  = "secondary-container-${lower(var.ARM_SECONDARY_REGION)}"
+  name                  = "secondary-container"
   storage_account_id    = azurerm_storage_account.secondary.id
   container_access_type = "private"
 }
