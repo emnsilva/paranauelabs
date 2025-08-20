43f14f8f641d23398dae89b6033fade2ff3095df
Emerson <emnsilva@hotmail.com>
2025-08-20 15:27:14 -0300

    Update providers.tf

--- #T - Terraform Airlines/providers.tf
+++ #T - Terraform Airlines/providers.tf
@@ -1,7 +1,24 @@
+# CONFIGURAÇÃO DE PROVEDORES REQUERIDOS
+# Define quais "plugins" o Terraform precisa baixar e instalar
+# Cada provedor é um componente que sabe conversar com uma nuvem específica
 terraform {
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 6.9.0" }
-    azurerm = { source = "hashicorp/azurerm", version = "~> 4.40.0" }
-    google = { source = "hashicorp/google", version = "~> 6.48.0" }
+    # Provedor AWS - Amazon Web Services
+    aws = {
+      source  = "hashicorp/aws"    # Fonte oficial da HashiCorp
+      version = "~> 6.9.0"         # Versão aproximadamente 6.9.0
+    }
+    
+    # Provedor Azure - Microsoft Azure
+    azurerm = {
+      source  = "hashicorp/azurerm" # Fonte oficial da HashiCorp
+      version = "~> 4.40.0"         # Versão aproximadamente 4.40.0
+    }
+    
+    # Provedor Google - Google Cloud Platform
+    google = {
+      source  = "hashicorp/google"  # Fonte oficial da HashiCorp
+      version = "~> 6.48.0"         # Versão aproximadamente 6.48.0
+    }
   }
 }
