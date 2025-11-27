b7c1767e000d579283f7c689b65ff995c01bea9d
emnsilva <emnsilva@gmail.com>
2025-11-27 11:08:34 -0300

    Ajuste nos módulos pra separar os buckets primário e secundário

---
+++ #হ - O hangar de módulos da Terraform Airlines/módulos/s3-module/main-secondary.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "secondary" {
+  provider      = aws.secondary
+  bucket        = var.bucket_name
+  force_destroy = var.force_destroy
+  tags          = var.tags
+}
+
+resource "aws_s3_bucket_public_access_block" "secondary" {
+  provider = aws.secondary
+  bucket   = aws_s3_bucket.secondary.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
