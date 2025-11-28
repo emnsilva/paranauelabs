b2d05c0ce19dca679220a58208752f3c6ac25527
Emerson <emnsilva@hotmail.com>
2025-11-28 08:10:42 -0300

    Rename S3 bucket resources for clarity

--- #হ - O hangar de módulos da Terraform Airlines/modules/s3-module/main-primary.tf
+++ #হ - O hangar de módulos da Terraform Airlines/modules/s3-module/main-primary.tf
@@ -1,13 +1,13 @@
-resource "aws_s3_bucket" "this" {
-  provider      = aws[var.provider_alias]
+resource "aws_s3_bucket" "primary" {
+  provider      = aws.primary
   bucket        = var.bucket_name
   force_destroy = var.force_destroy
   tags          = var.tags
 }
 
-resource "aws_s3_bucket_public_access_block" "this" {
-  provider = aws[var.provider_alias]
-  bucket   = aws_s3_bucket.this.id
+resource "aws_s3_bucket_public_access_block" "primary" {
+  provider = aws.primary
+  bucket   = aws_s3_bucket.primary.id
 
   block_public_acls       = true
   block_public_policy     = true
