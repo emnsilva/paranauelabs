6be8bed3fe290d478df6773e53fe9c98541f8e18
emnsilva <emnsilva@gmail.com>
2025-11-27 22:00:11 -0300

    Ajuste no deploy.tf

--- #হ - O hangar de módulos da Terraform Airlines/deploy.tf
+++ #হ - O hangar de módulos da Terraform Airlines/deploy.tf
@@ -8,12 +8,8 @@ variable "ARM_SECONDARY_REGION" {}
 variable "GCP_PRIMARY_REGION" {}
 variable "GCP_SECONDARY_REGION" {}
 variable "GCP_PROJECT" {}
-variable "ARM_SUBSCRIPTION_ID" {
-  default = "temp-fix"
-}
-variable "ARM_TENANT_ID" {
-  default = "temp-fix"
-}
+variable "ARM_SUBSCRIPTION_ID" {default = "temp-fix"}
+variable "ARM_TENANT_ID" {default = "temp-fix"}
 variable "GOOGLE_CREDENTIALS_B64" {}
 
 # Random suffix para evitar conflitos de nomes (coloque no TOPO)
