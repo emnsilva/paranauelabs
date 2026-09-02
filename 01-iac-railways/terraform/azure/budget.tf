1bf7af28bcfa2db76ec54c8724815120c31cf042
Emershow <39090553+emnsilva@users.noreply.github.com>
2026-09-02 12:41:46 -0300

    Ajuste na budget do Azure para o mês de setembro de 2026.

--- 01-iac-railways/terraform/azure/budget.tf
+++ 01-iac-railways/terraform/azure/budget.tf
@@ -11,7 +11,7 @@ resource "azurerm_consumption_budget_subscription" "paranauelabs" {
   time_grain = "Monthly"
 
   time_period {
-    start_date = "2024-01-01T00:00:00Z"
+    start_date = "2026-09-02T12:45s:00Z"
   }
 
   notification {
